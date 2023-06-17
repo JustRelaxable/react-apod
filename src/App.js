32517d94e6b6c94c13e3f6df315facd0d2d789ed
Taha Sökmen <resimli1000@gmail.com>
2023-06-17 14:05:21 +0300

    Implement basic fetch API functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import "./App.css";
+import RandomPicture from "./Components/RandomPicture";
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="content">
+      <RandomPicture></RandomPicture>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/RandomPicture.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import "../StyleModules/RandomPicture.css";
+
+export default function RandomPicture({}) {
+  const [randomRequestCount, setRandomRequestCount] = useState(0);
+  const [explanation, setExplanation] = useState("");
+  const [date, setDate] = useState("");
+  const [imageURL, setImageURL] = useState("");
+  const [fullImageURL, setFullImageURL] = useState("");
+  const [title, setTitle] = useState("");
+  const [dataReceived, setDataReceived] = useState(false);
+
+  useEffect(() => {
+    setDataReceived(false);
+    fetch(
+      "https://api.nasa.gov/planetary/apod?api_key=xVAwzJcqMnRhu5ikdWqjghdIKf65P3GkR6V97t0c&count=1"
+    )
+      .then((x) => x.json())
+      .then((x) => x[0])
+      .then((x) => {
+        setDate(x.date);
+        setExplanation(x.explanation);
+        setImageURL(x.url);
+        setTitle(x.title);
+        setFullImageURL(x.hdurl);
+        setDataReceived(true);
+        console.log(x);
+      });
+  }, [randomRequestCount]);
+
+  return (
+    <div className="random-picture-container">
+      {!dataReceived && <p>Waiting to receive data</p>}
+      {dataReceived && (
+        <>
+          <img src={imageURL} className="random-picture-picture"></img>
+          <h2>
+            {title} - {date}
+          </h2>
+          <p>{explanation}</p>
+          <div>
+            <button
+              onClick={() => setRandomRequestCount(randomRequestCount + 1)}
+            >
+              Another Random Event!
+            </button>
+            <button
+              onClick={() => {
+                window.open(fullImageURL, "_blank");
+              }}
+            >
+              Load Full Size Image
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
